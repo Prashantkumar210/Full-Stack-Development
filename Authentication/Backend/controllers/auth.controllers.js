03da7bc50ae483d5ca8d2150892d35c22ae74847
Prashant Kumar <prashant133191@gmail.com>
2026-02-21 20:56:36 +0530

    Authentication

--- Authentication/Backend/controllers/auth.controllers.js
+++ Authentication/Backend/controllers/auth.controllers.js
@@ -1,3 +1,4 @@
+import generateToken from "../config/token.js"
 import User from "../model/user.model.js"
 import bcrypt from "bcryptjs"
 
@@ -10,6 +11,8 @@ export const signUp =async(req,res)=>{
         if(!firstName || !lastName || !email || !password || !userName){
             return res.status(400).json({messaage:"send all details"})
         }
+
+       
 
         let existUser = await User.findOne({email})
         if(existUser){
@@ -25,6 +28,24 @@ export const signUp =async(req,res)=>{
             email,
             password:hassedPasswrd
         })
+
+        let token;
+        try{
+            token = generateToken(user._id)
+        }catch(error){
+            console.log(error)
+        }
+        
+
+        res.cookie("token", token, {
+            httpOnly:true,
+            secure:process.env.NODE_ENVIRONMENT == "producation",
+            sameSite:"strict",
+            maxAge:7*24*60*60*1000
+        })
+
+        console.log(user)
+
         return res.status(200).json({user:{
             firstName,
             lastName,
